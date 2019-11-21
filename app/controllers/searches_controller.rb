863011a8143c703914250965fc10854aab02d907
Carrie <carrie.g.crow@gmail.com>
2019-11-21 20:21:46 +0000

    Submit form data from user

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,13 +3,13 @@ class SearchesController < ApplicationController
   end
 
   def org_search
-    #Submit entered parameters from the view to organizations.json
+
   end
 private
   def parse_json
     #helper method to parse the three files before search
   end
-  
+
   def org_params
     #Allow user to only enter acceptable parameters
   end
