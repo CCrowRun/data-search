8afe8c754c883dc27969e21cf18cfe450ab972c2
Carrie <carrie.g.crow@gmail.com>
2020-01-21 19:33:41 +0000

    Removed byebug troubleshooter

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,12 +6,10 @@ class SearchesController < ApplicationController
     search_file_contents = File.read("#{Rails.root}/app/data/organizations.json")
     data = JSON.parse(search_file_contents)
     result = data.select { |org| org[org_params["org_criteria"]].to_s == org_params["org_value"]}
-    byebug
     render json: result
-
   end
-private
 
+private
   def org_params
     params.permit(:org_criteria, :org_value)
   end
