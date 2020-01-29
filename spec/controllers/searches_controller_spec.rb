b6ca277fd1c33e62f1616d0f351fa1de207dea12
Carrie <carrie.g.crow@gmail.com>
2020-01-29 20:34:15 +0000

    Created tests for searches controller

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -7,11 +7,23 @@ RSpec.describe SearchesController, type: :controller do
 
       expect(response).to have_http_status(:success)
     end
+
+    it "should submit search for Organization ID" do
+      get :org_search, params: { name: "Enhaze" }
+
+      puts response_body
+
+      expect(response).to have_http_status :success
+    end
+    
     it "should submit search for Organization ID" do
 
       get :org_search, params: { _id: 1 }
 
       expect(response).to have_http_status :success
+    end
+    def response_body
+      JSON.parse(response.body)
     end
   end
 end
